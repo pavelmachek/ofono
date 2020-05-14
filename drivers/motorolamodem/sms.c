9ca15455c2173b5909527aa6695f4bbd7aca313c
Pavel <pavel@ucw.cz>
2020-05-14 18:47:46 +0200

    d4: sms: Remove some more unused code.

--- drivers/motorolamodem/sms.c
+++ drivers/motorolamodem/sms.c
@@ -156,53 +156,14 @@ static inline void motorola_ack_delivery(struct ofono_sms *sms)
 	char buf[256];
 
 	DBG("");
-	  if (0) {
-		/* Should be a safe fallback, documented, and works for me.
-		   Does not work for Tony. */
-		/* No, it does not work. CNMA=0 does not really acknowledge
-		   the messages, they come back later */
-		snprintf(buf, sizeof(buf), "U0000AT+CNMA=0");
-	  } else {
-	  	/* SMSes seem to be acknowledged, but then they
-		   somehow reappear later? */
-		/* Neither GCNMA=1 nor GCNMA=0 acknowledges the message, it is delivered over and over. */
-		snprintf(buf, sizeof(buf), "U0000AT+GCNMA=1");
-		/* GCNMA=1 on _outsms_ may be doing the trick?
-		   Tony does GCNMA on insms. (and suggest CNMA=0,0; sometimes) */
-	  }
+	snprintf(buf, sizeof(buf), "U0000AT+GCNMA=1");
+	/* GCNMA=1 on _outsms_ may be doing the trick?
+	   Tony does GCNMA on insms. (and suggest CNMA=0,0; sometimes) */
 
-	  if (0) {
-		  g_mot_chat_send(data->chat, buf, none_prefix, at_cnma_cb, NULL, NULL);
-	  } else {
-		  strcat(buf, "\r");
-		  g_at_io_write(data->send_chat->parent->io, buf, strlen(buf));
-		  g_io_channel_flush(data->send_chat->parent->io->channel, NULL);
-#if 0
-		  g_at_io_write(data->chat->parent->io, buf, strlen(buf));
-		  g_io_channel_flush(data->chat->parent->io->channel, NULL);
-		  snprintf(buf, sizeof(buf), "U0000AT+FOO");
-		  strcat(buf, "\r");
-		  g_at_io_write(data->chat->parent->io, buf, strlen(buf));
-		  g_io_channel_flush(data->chat->parent->io->channel, NULL);
-		  g_at_io_write(data->send_chat->parent->io, buf, strlen(buf));
-		  g_io_channel_flush(data->send_chat->parent->io->channel, NULL);
-		  snprintf(buf, sizeof(buf), "U0000AT+CNMA=0");
-		  strcat(buf, "\r");
-		  g_at_io_write(data->chat->parent->io, buf, strlen(buf));
-		  g_io_channel_flush(data->chat->parent->io->channel, NULL);
-		  g_at_io_write(data->send_chat->parent->io, buf, strlen(buf));
-		  g_io_channel_flush(data->send_chat->parent->io->channel, NULL);
-		  snprintf(buf, sizeof(buf), "U0000AT+CNMA=0,0");
-		  strcat(buf, "\r");
-		  g_at_io_write(data->send_chat->parent->io, buf, strlen(buf));
-		  g_io_channel_flush(data->send_chat->parent->io->channel, NULL);
-		  g_at_io_write(data->chat->parent->io, buf, strlen(buf));
-		  g_io_channel_flush(data->chat->parent->io->channel, NULL);
-#endif
-	  }
+	strcat(buf, "\r");
+	g_at_io_write(data->send_chat->parent->io, buf, strlen(buf));
+	g_io_channel_flush(data->send_chat->parent->io->channel, NULL);
 }
-
-/*  */
 
 static void got_hex_pdu(struct ofono_sms *sms, const char *hexpdu)
 {
@@ -211,8 +172,8 @@ static void got_hex_pdu(struct ofono_sms *sms, const char *hexpdu)
   	unsigned char pdu[176];
 
 	if (hexpdu[0] == '~') {
-	  printf("PDU will be on next line?\n");
-	  return;
+		printf("PDU will be on next line?\n");
+		return;
 	}
 
 	if (strlen(hexpdu) > sizeof(pdu) * 2) {
@@ -220,18 +181,11 @@ static void got_hex_pdu(struct ofono_sms *sms, const char *hexpdu)
 		return;
 	}
 
-	/*	for (tpdu_len = 0; tpdu_len < 159; tpdu_len++) */
-	  {
-	  printf("tpdu_len: %d\n", tpdu_len);
 	/* Decode pdu and notify about new SMS status report */
-	  decode_hex_own_buf(hexpdu, -1, &pdu_len, 0, pdu);
-	  tpdu_len = pdu_len - pdu[0] - 1; /* Matches my guess, and matches mbimodem */
-	  DBG("Got new Status-Report PDU via CDS: %s, %d", hexpdu, tpdu_len);
-	  ofono_sms_deliver_notify(sms, pdu, pdu_len, tpdu_len);
-	}
-#if 0
-	  construct_ack_pdu(sms);
-#endif
+	decode_hex_own_buf(hexpdu, -1, &pdu_len, 0, pdu);
+	tpdu_len = pdu_len - pdu[0] - 1; /* Matches my guess, and matches mbimodem */
+	DBG("Got new Status-Report PDU via CDS: %s, %d", hexpdu, tpdu_len);
+	ofono_sms_deliver_notify(sms, pdu, pdu_len, tpdu_len);
 }
 
 static void insms_notify(GAtResult *result, gpointer user_data)
@@ -282,46 +236,10 @@ static int motorola_sms_probe(struct ofono_sms *sms, unsigned int vendor,
 	g_mot_chat_register(data->chat, "U0000~+GCMT", insms_notify, FALSE, sms, NULL);
 	g_mot_chat_register(data->send_chat, "U0000+GCMGS", motorola_cmgs_cb, FALSE, sms, NULL);
 
-#if 0
-	/* Tony says this acks sms, I don't see the effect */
-	g_mot_chat_send(data->chat, "U0000AT+GCNMA=1", csms_prefix,
-			motorola_csms_query_cb, sms, NULL);
-	/* Weird. Now it says "+CMS=305". I'm pretty sure it did not do that before? */
-	g_mot_chat_send(data->chat, "U0000AT+CNMA=0,0", csms_prefix,
-			motorola_csms_query_cb, sms, NULL);
-#endif
-
-#if 0
-	/*	~+GCMT=318\r
-                          07912470338016000404B933330011911010122402409BC4B7589E0791CB6E16686E2F83D0E539FB0D72A7D7EF761DE42ECFC965765D4D2FB340613A08FD06B9D36BF21BE42EB7EBFA3248EF2ED7F569BA0B640DCFCB207479CE7E83D620B83C8D6687E765771A447E839A6F719AED1AEB41EA32E8169BD968305018046787E969105CFE068DD373F61B749BD5723498EC367381ACE139A8F916A7D9AEB11E 
-AT+GCNMA=1
-+CMS=305
-AT+GCNMA=1
-+GCMS=305
-07912470338016000004B933330011911061218380409BC4B7589E0791CB6E16686E2F83D0E539FB0D72A7D7EF761DE4E
-
-07912470338016000004B933330011911061218380409BC4B7589E0791CB6E16686E2F83D0E539FB0D72A7D7EF761DE42
-ECFC965765D4D2FB340613A08FD06B9D36BF21BE42EB7EBFA3248EF2ED7F569BA0B640DCFCB207479CE7E83D620B83C8D
-6687E765771A447E839A6F719AED1AEB41EA322886CBD16C335018046787E969105CFE068DD373F61B749BD5723498EC3
-67381ACE139A8F916A7D9AEB11E
-
-AT+GCNMA=1
-0000
-:ERROR=4
-AT+GCNMA=1
-+GCNMA=OK
-
-  
-
-
-	*/
-	got_hex_pdu(sms, "07912470338016000404B933330011911010127042409BC4B7589E0791CB6E16686E2F83D0E539FB0D72A7D7EF761DE42ECFC965765D4D2FB340613A08FD06B9D36BF21BE42EB7EBFA3248EF2ED7F569BA0B640DCFCB207479CE7E83D620B83C8D6687E765771A447E839A6F719AED1AEB41EA326846C3E564315018046787E969105CFE068DD373F61B749BD5723498EC367381ACE139A8F916A7D9AEB11E");
-
 	/* List of error codes seems to be at
 
 	   https://www.developershome.com/sms/resultCodes2.asp#16.2.1.1
 	 */
-#endif	
 	return 0;
 }
 
@@ -336,24 +254,11 @@ static void motorola_sms_remove(struct ofono_sms *sms)
 	ofono_sms_set_data(sms, NULL);
 }
 
-#if 0
-static void motorola_unimpl(void)
-{
-	DBG("");
-}
-#endif
-
 static const struct ofono_sms_driver driver = {
 	.name		= "motorolamodem",
 	.probe		= motorola_sms_probe,
 	.remove		= motorola_sms_remove,
 	.submit		= motorola_cmgs,
-#if 0
-	.sca_query	= motorola_unimpl,
-	.sca_set	= motorola_unimpl,
-	.bearer_query	= motorola_unimpl,
-	.bearer_set	= motorola_unimpl,
-#endif
 };
 
 void motorola_sms_init(void)
